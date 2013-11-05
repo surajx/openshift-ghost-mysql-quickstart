e6d23002af92ceccb5fe7bec56ef9d084bed0912
Corey Daley <cdaley@redhat.com>
2013-11-05 14:26:16 -0500

    adding openshift environment variable for mysql port for scalable applications

--- config.js
+++ config.js
@@ -50,6 +50,7 @@ config = {
             client: 'mysql',
             connection: {
   			  host: process.env.OPENSHIFT_MYSQL_DB_HOST,
+			  port: process.env.OPENSHIFT_MYSQL_DB_PORT,
   			  user: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
   			  password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
   			  database: process.env.OPENSHIFT_APP_NAME,
